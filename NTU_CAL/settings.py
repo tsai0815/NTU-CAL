22475c9aa999ef484a2e6377c8d92790a46160a5
tsai0815 <tsai815@MSI>
2024-05-24 22:50:33 +0800

    typo

--- NTU_CAL/settings.py
+++ NTU_CAL/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-cSTATIC_ROOT = BASE_DIR / 'productionfiles'
+STATIC_ROOT = BASE_DIR / 'productionfiles'
 
 STATIC_URL = 'static/'
 
