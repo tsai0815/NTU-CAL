95b637ac5c71ce346108d73f6b39103108648827
tsai0815 <tsai815@MSI>
2024-05-27 10:28:34 +0800

    create Question model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=50)
+    category = models.CharField(max_length=30)
+    description = models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.title} {self.category} {self.description}"
