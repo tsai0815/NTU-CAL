14885b1fc24f2bcf906cfe27e92a48aa99147a62
Weichi Wu <6387385+weichiwu@users.noreply.github.com>
2024-05-29 00:35:20 +0800

    update login cache

--- forum/urls.py
+++ forum/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from . import views
 
@@ -12,3 +14,6 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     # path('forum/', views.forum, name='forum'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
