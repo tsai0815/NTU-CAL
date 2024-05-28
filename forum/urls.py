a6f80157fded6120bd8a7d8e9b9b874c02a4defd
Weichi Wu <6387385+weichiwu@users.noreply.github.com>
2024-05-28 23:39:20 +0800

    update login layout

--- forum/urls.py
+++ forum/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('aboutus/', views.aboutus, name='aboutus'),
     path('account-center/', views.account_center, name='account-center'),
     path('calculus/', views.calculus, name='calculus'),
+    path('login/', views.login, name='login'),
     # path('forum/', views.forum, name='forum'),
 ]
